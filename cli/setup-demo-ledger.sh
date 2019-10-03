7c524f452b5bcea002fe38b858efe7042a82044d
Nick Chapman <nick.chapman@digitalasset.com>
2019-10-03 11:57:49 +0100

    cli, demo setup, use explicit port & sleep 2

--- cli/setup-demo-ledger.sh
+++ cli/setup-demo-ledger.sh
@@ -6,12 +6,15 @@ set -euo pipefail
 
 cd "$(dirname ${BASH_SOURCE[0]})"
 
+host='localhost'
+port=6556
+
 stack build
-CLI=$(stack exec which davl-cli)
+CLI="$(stack exec which davl-cli) --host $host --port $port"
 
-(cd ../project/V1; daml deploy)
+(cd ../project/V1; daml deploy --host $host --port $port)
 
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 give Bob
 give Bob
 give Bob
@@ -20,69 +23,69 @@ give Charlie
 give Charlie
 EOF
 
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 request 17749
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 request 17750
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Bob <<EOF
+sleep 2; $CLI Bob <<EOF
 request 17751
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
 
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 request 17756
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 request 17757
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 claim Alice
 EOF
-sleep 1; $CLI Charlie <<EOF
+sleep 2; $CLI Charlie <<EOF
 request 17758
 EOF
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 approve 1
 EOF
 
 
-sleep 1; $CLI Alice <<EOF
+sleep 2; $CLI Alice <<EOF
 summary
 vacations
 EOF
