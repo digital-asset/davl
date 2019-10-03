d2ff4364b6f30884232ca8475de5eb2f80f72249
Nick Chapman <nick.chapman@digitalasset.com>
2019-10-03 08:01:21 +0100

    add 27x sleep1 to test case1

--- cli/testcases/case1.sh
+++ cli/testcases/case1.sh
@@ -6,44 +6,44 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 CLI=$(stack exec which davl-cli)
 
 # Alice gifts 2 days of holiday allocation to each of Bob and Charlie
-echo 'give Bob' | $CLI Alice
-echo 'give Bob' | $CLI Alice
-echo 'give Charlie' | $CLI Alice
-echo 'give Charlie' | $CLI Alice
-echo 'summary' | $CLI Alice
+sleep 1; echo 'give Bob' | $CLI Alice
+sleep 1; echo 'give Bob' | $CLI Alice
+sleep 1; echo 'give Charlie' | $CLI Alice
+sleep 1; echo 'give Charlie' | $CLI Alice
+sleep 1; echo 'summary' | $CLI Alice
 
 # Bob claims one day of his allocation
-echo 'history' | $CLI Bob
-echo 'summary' | $CLI Bob
-echo 'claim Alice' | $CLI Bob
+sleep 1; echo 'history' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Bob
+sleep 1; echo 'claim Alice' | $CLI Bob
 
 # What does Alice see?
-echo 'history' | $CLI Alice
-echo 'summary' | $CLI Alice
+sleep 1; echo 'history' | $CLI Alice
+sleep 1; echo 'summary' | $CLI Alice
 
 # Bob make a holiday request using his single allocation
-echo 'request 100' | $CLI Bob
-echo 'summary' | $CLI Bob
+sleep 1; echo 'request 100' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Bob
 
 # Alice sees the request, and denies it
-echo 'summary' | $CLI Alice
-echo 'pending' | $CLI Alice
-echo 'deny 1 not-that-day-please' | $CLI Alice
-echo 'summary' | $CLI Alice
+sleep 1; echo 'summary' | $CLI Alice
+sleep 1; echo 'pending' | $CLI Alice
+sleep 1; echo 'deny 1 not-that-day-please' | $CLI Alice
+sleep 1; echo 'summary' | $CLI Alice
 
 # Bob sees the denial, any make a request for a different day
-echo 'summary' | $CLI Bob
-echo 'denials' | $CLI Bob
-echo 'request 200' | $CLI Bob
-echo 'summary' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Bob
+sleep 1; echo 'denials' | $CLI Bob
+sleep 1; echo 'request 200' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Bob
 
 # Alice sees the new request and accepts it
-echo 'summary' | $CLI Alice
-echo 'pending' | $CLI Alice
-echo 'approve 1' | $CLI Alice
-echo 'summary' | $CLI Alice
-echo 'vacations' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Alice
+sleep 1; echo 'pending' | $CLI Alice
+sleep 1; echo 'approve 1' | $CLI Alice
+sleep 1; echo 'summary' | $CLI Alice
+sleep 1; echo 'vacations' | $CLI Bob
 
 # Bob sees his scheduled vacation day
-echo 'summary' | $CLI Bob
-echo 'vacations' | $CLI Bob
+sleep 1; echo 'summary' | $CLI Bob
+sleep 1; echo 'vacations' | $CLI Bob
