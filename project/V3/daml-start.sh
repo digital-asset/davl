012afc729e912c9fbc3224ce4338a2193c62e070
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-11-01 13:26:21 -0400

    Add simple version 3 of DAML model

---
+++ project/V3/daml-start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+daml start \
+  --open-browser no \
+  --sandbox-option --wall-clock-time \
+  --sandbox-option --ledgerid \
+  --sandbox-option "davl" \
+  --sandbox-option --sql-backend-jdbcurl \
+  --sandbox-option "jdbc:postgresql://localhost/davl?user=davl&password=davl" \
+  $*
