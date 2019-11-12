34dabdf573b257870b6c5e0a8323a44f8164ccd2
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-11-12 23:33:16 +0100

    Prepare "ledger bindings" for automatic generation
    Make the code that should be automatically generated from the DAML easier to
    generate.

--- ui/src/daml/v3/DAVL.ts
+++ ui/src/daml/v3/DAVL.ts
@@ -1,106 +1,112 @@
 /* eslint-disable @typescript-eslint/class-name-casing */
 /* eslint-disable @typescript-eslint/camelcase */
-import { Party, Template, Choice, party, int, Int, Date, date, TemplateId } from '../../ledger/types';
+import * as daml from '../../ledger/types';
 import { object } from '@mojotech/json-type-validation';
 import packageId from './packageId';
 
 const moduleName = 'DAVL';
-const templateId = (entityName: string): TemplateId => ({packageId, moduleName, entityName});
+const templateId = (entityName: string): daml.TemplateId => ({packageId, moduleName, entityName});
 
 export type EmployeeRole_RequestVacation = {
-  fromDate: Date;
-  toDate: Date;
+  fromDate: daml.Date;
+  toDate: daml.Date;
 }
 
-export const EmployeeRole_RequestVacation: Choice<EmployeeRole, EmployeeRole_RequestVacation> = {
-  template: undefined as unknown as typeof EmployeeRole,
-  choiceName: "EmployeeRole_RequestVacation",
-  decoder: () => object({fromDate: date(), toDate: date()}),
+export const EmployeeRole_RequestVacation: daml.Serializable<EmployeeRole_RequestVacation> = {
+  decoder: () => object({
+    fromDate: daml.Date.decoder(),
+    toDate: daml.Date.decoder(),
+  }),
 }
 
 export type EmployeeRole = {
-  employee: Party;
-  company: Party;
-  boss: Party;
+  employee: daml.Party;
+  company: daml.Party;
+  boss: daml.Party;
 }
 
-export const EmployeeRole: Template<EmployeeRole> & {
-  RequestVacation: Choice<EmployeeRole, EmployeeRole_RequestVacation>;
+export const EmployeeRole: daml.Template<EmployeeRole> & {
+  RequestVacation: daml.Choice<EmployeeRole, EmployeeRole_RequestVacation>;
 } = {
   templateId: templateId("EmployeeRole"),
   decoder: () => object({
-    employee: party(),
-    company: party(),
-    boss: party(),
+    employee: daml.Party.decoder(),
+    company: daml.Party.decoder(),
+    boss: daml.Party.decoder(),
   }),
-  RequestVacation: EmployeeRole_RequestVacation,
+  RequestVacation: {
+    template: undefined as unknown as daml.Template<EmployeeRole>,
+    choiceName: "EmployeeRole_RequestVacation",
+    decoder: EmployeeRole_RequestVacation.decoder,
+  }
 }
 
-EmployeeRole_RequestVacation.template = EmployeeRole;
+EmployeeRole.RequestVacation.template = EmployeeRole;
 
 
 export type EmployeeProposal_Accept = {}
 
-export const EmployeeProposal_Accept: Choice<EmployeeProposal, EmployeeProposal_Accept> = {
-  template: undefined as unknown as typeof EmployeeProposal,
-  choiceName: "EmployeeProposal_Accept",
+export const EmployeeProposal_Accept: daml.Serializable<EmployeeProposal_Accept> = {
   decoder: () => object({}),
 }
 
 export type EmployeeProposal = {
   employeeRole: EmployeeRole;
-  vacationDays: Int;
+  vacationDays: daml.Int;
 }
 
-export const EmployeeProposal: Template<EmployeeProposal> & {
-  Accept: Choice<EmployeeProposal, EmployeeProposal_Accept>;
+export const EmployeeProposal: daml.Template<EmployeeProposal> & {
+  Accept: daml.Choice<EmployeeProposal, EmployeeProposal_Accept>;
 } = {
   templateId: templateId("EmployeeProposal"),
   decoder: () => object({
     employeeRole: EmployeeRole.decoder(),
-    vacationDays: int(),
+    vacationDays: daml.Int.decoder(),
   }),
-  Accept: EmployeeProposal_Accept,
+  Accept: {
+    template: undefined as unknown as daml.Template<EmployeeProposal>,
+    choiceName: "EmployeeProposal_Accept",
+    decoder: EmployeeProposal_Accept.decoder,
+  }
 }
 
-EmployeeProposal_Accept.template = EmployeeProposal;
+EmployeeProposal.Accept.template = EmployeeProposal;
 
 
 export type EmployeeVacationAllocation = {
   employeeRole: EmployeeRole;
-  remainingDays: Int;
+  remainingDays: daml.Int;
 }
 
-export const EmployeeVacationAllocation: Template<EmployeeVacationAllocation> = {
+export const EmployeeVacationAllocation: daml.Template<EmployeeVacationAllocation> = {
   templateId: templateId("EmployeeVacationAllocation"),
   decoder: () => object({
     employeeRole: EmployeeRole.decoder(),
-    remainingDays: int(),
+    remainingDays: daml.Int.decoder(),
   }),
 }
 
 
 export type Vacation = {
   employeeRole: EmployeeRole;
-  fromDate: Date;
-  toDate: Date;
+  fromDate: daml.Date;
+  toDate: daml.Date;
 }
 
-export const Vacation: Template<Vacation> = {
+export const Vacation: daml.Template<Vacation> & {
+} = {
   templateId: templateId("Vacation"),
   decoder: () => object({
     employeeRole: EmployeeRole.decoder(),
-    fromDate: date(),
-    toDate: date(),
+    fromDate: daml.Date.decoder(),
+    toDate: daml.Date.decoder(),
   }),
 }
 
 
 export type VacationRequest_Accept = {}
 
-export const VacationRequest_Accept: Choice<VacationRequest, VacationRequest_Accept> = {
-  template: undefined as unknown as typeof VacationRequest,
-  choiceName: "VacationRequest_Accept",
+export const VacationRequest_Accept: daml.Serializable<VacationRequest_Accept> = {
   decoder: () => object({}),
 }
 
@@ -108,14 +114,18 @@ export type VacationRequest = {
   vacation: Vacation;
 }
 
-export const VacationRequest: Template<VacationRequest> & {
-  Accept: Choice<VacationRequest, VacationRequest_Accept>;
+export const VacationRequest: daml.Template<VacationRequest> & {
+  Accept: daml.Choice<VacationRequest, VacationRequest_Accept>;
 } = {
   templateId: templateId("VacationRequest"),
   decoder: () => object({
     vacation: Vacation.decoder(),
   }),
-  Accept: VacationRequest_Accept,
+  Accept: {
+    template: undefined as unknown as daml.Template<VacationRequest>,
+    choiceName: "VacationRequest_Accept",
+    decoder: VacationRequest_Accept.decoder,
+  }
 }
 
-VacationRequest_Accept.template = VacationRequest;
+VacationRequest.Accept.template = VacationRequest;
