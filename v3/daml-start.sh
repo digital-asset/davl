8b1c0b5fc29936c5dfbc87771368071f933c6ffc
Martin Huschenbett <martin.huschenbett@posteo.me>
2019-11-17 18:14:01 +0100

    Use same ledger id in dev as in prod (#67)
    Otherwise development will become very painful...

--- v3/daml-start.sh
+++ v3/daml-start.sh
@@ -3,7 +3,7 @@ daml start \
   --open-browser no \
   --sandbox-option --wall-clock-time \
   --sandbox-option --ledgerid \
-  --sandbox-option "davl" \
+  --sandbox-option "DAVL" \
   # --sandbox-option --sql-backend-jdbcurl \
   # --sandbox-option "jdbc:postgresql://localhost/davl?user=davl&password=davl" \
   $*
