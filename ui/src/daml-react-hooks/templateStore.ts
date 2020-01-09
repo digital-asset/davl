9d98494537ae0945d8b630375574b50e0cf1ef03
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-01-09 13:49:03 +0100

    daml-react-hooks: Add contract key type parameter (#112)

--- ui/src/daml-react-hooks/templateStore.ts
+++ ui/src/daml-react-hooks/templateStore.ts
@@ -1,34 +1,34 @@
 import * as immutable from 'immutable';
 import { CreateEvent, Query } from '@digitalasset/daml-ledger-fetch';
 
-export type QueryResult<T extends object> = {
-  contracts: CreateEvent<T>[];
+export type QueryResult<T extends object, K> = {
+  contracts: CreateEvent<T, K>[];
   loading: boolean;
 }
 
-export type Store<T extends object> = {
-  queryResults: immutable.Map<Query<T>, QueryResult<T>>;
+export type Store<T extends object, K> = {
+  queryResults: immutable.Map<Query<T>, QueryResult<T, K>>;
 }
 
-export const emptyQueryResult = <T extends object>(): QueryResult<T> => ({
+export const emptyQueryResult = <T extends object, K>(): QueryResult<T, K> => ({
   contracts: [],
   loading: false,
 });
 
-export const empty = <T extends object>(): Store<T> => ({
+export const empty = <T extends object, K>(): Store<T, K> => ({
   queryResults: immutable.Map(),
 });
 
-export const setAllLoading = <T extends object>(store: Store<T>): Store<T> => ({
+export const setAllLoading = <T extends object, K>(store: Store<T, K>): Store<T, K> => ({
   queryResults: store.queryResults.map((res) => ({...res, loading: true})),
 })
 
-export const setQueryLoading = <T extends object>(store: Store<T>, query: Query<T>): Store<T> => ({
+export const setQueryLoading = <T extends object, K>(store: Store<T, K>, query: Query<T>): Store<T, K> => ({
   ...store,
   queryResults: store.queryResults.update(query, (res = emptyQueryResult()) => ({...res, loading: true})),
 })
 
-export const setQueryResult = <T extends object>(store: Store<T>, query: Query<T>, contracts: CreateEvent<T>[]): Store<T> => ({
+export const setQueryResult = <T extends object, K>(store: Store<T, K>, query: Query<T>, contracts: CreateEvent<T, K>[]): Store<T, K> => ({
   ...store,
   queryResults: store.queryResults.set(query, {contracts, loading: false})
 });
