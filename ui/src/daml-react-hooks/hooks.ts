accd317aa847126c1b9afa6eabaa422a50ecb978
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-06 13:28:55 +0100

    Add and use a React hook calling the streaming query JSON API
    This also allows us to entirely rewrite the existing `useQuery` and
    `useFetchByKey`. There's no more need to have the Redux reducer and one
    central store since users who want updates can use `useStreamQuery` instead.
    This removes a lot of complexity. Now, every instance of the hooks has
    its own, very simple data store implemented using `useState`.
    
    Unfortunately, `useStreamFetchByKey` is not yet possible since the
    correspoding endpoint in the JSON API. Thus, to see the updates to a
    contract key an explicit reload is still necessary.
    
    All of this has still plenty of quirks when it comes to exceptions and
    asynchronicity. However, nothing of this was better before. I'll address
    it in a bunch of follow-up PRs.

--- ui/src/daml-react-hooks/hooks.ts
+++ ui/src/daml-react-hooks/hooks.ts
@@ -1,15 +1,12 @@
 // Copyright (c) 2020 The DAML Authors. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Template, Choice, ContractId } from "@daml/types";
-import { Query, CreateEvent } from '@daml/ledger';
-import { useEffect, useMemo, useState, useContext } from "react";
-import * as LedgerStore from './ledgerStore';
-import * as TemplateStore from './templateStore';
-import { setQueryLoading, setQueryResult, setFetchByKeyLoading, setFetchByKeyResult, addEvents } from "./reducer";
-import { DamlLedgerState, DamlLedgerContext } from './context';
+import { Choice, ContractId, Template } from "@daml/types";
+import { CreateEvent, Query, Event } from '@daml/ledger';
+import { useState, useContext, useEffect } from "react";
+import { DamlLedgerState, DamlLedgerContext } from './context'
 
-export const useDamlState = (): DamlLedgerState => {
+const useDamlState = (): DamlLedgerState => {
   const state = useContext(DamlLedgerContext);
   if (!state) {
     throw Error("Trying to use DamlLedgerContext before initializing.")
@@ -22,46 +19,29 @@ export const useParty = () => {
   return state.party;
 }
 
-const loadQuery = async <T extends object>(state: DamlLedgerState, template: Template<T>, query: Query<T>) => {
-  state.dispatch(setQueryLoading(template, query));
-  const contracts = await state.ledger.query(template, query);
-  state.dispatch(setQueryResult(template, query, contracts));
-}
-
 export type QueryResult<T extends object, K> = {
   contracts: CreateEvent<T, K>[];
   loading: boolean;
 }
 
-// NOTE(MH): Since `{} !== {}`, we need a stable reference to `{}` for `useMemo`
-// to work in `useQuery` when using the default value for `queryFactory`.
-const emptyQuery = {};
-
 /// React Hook for a query against the `/contracts/search` endpoint of the JSON API.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useQuery = <T extends object, K>(template: Template<T, K>, queryFactory: () => Query<T> = () => emptyQuery as Query<T>, queryDeps?: readonly any[]): QueryResult<T, K> => {
+export function useQuery<T extends object, K>(template: Template<T, K>): QueryResult<T, K>
+export function useQuery<T extends object, K>(template: Template<T, K>, queryFactory: () => Query<T>, queryDeps: readonly unknown[]): QueryResult<T, K>
+export function useQuery<T extends object, K>(template: Template<T, K>, queryFactory?: () => Query<T>, queryDeps?: readonly unknown[]): QueryResult<T, K> {
   const state = useDamlState();
-  const query = useMemo(queryFactory, queryDeps);
-  const contracts = LedgerStore.getQueryResult(state.store, template, query);
+  const [result, setResult] = useState<QueryResult<T, K>>({contracts: [], loading: false});
   useEffect(() => {
-    if (contracts === undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      loadQuery(state, template, query);
-    }
-  }, [state, template, query, contracts]);
-  return contracts ?? TemplateStore.emptyQueryResult();
-}
-
-const loadFetchByKey = async <T extends object, K>(state: DamlLedgerState, template: Template<T, K>, key: K) => {
-  state.dispatch(setFetchByKeyLoading(template, key));
-  let contract;
-  if (key === undefined) {
-    console.error(`Calling useFetchByKey on template without a contract key: ${template}`);
-    contract = null;
-  } else {
-    contract = await state.ledger.lookupByKey(template, key as K extends undefined ? never : K);
-  }
-  state.dispatch(setFetchByKeyResult(template, key, contract));
+    setResult({contracts: [], loading: true});
+    const query = queryFactory ? queryFactory() : undefined;
+    const load = async () => {
+      const contracts = await state.ledger.query(template, query);
+      setResult({contracts, loading: false});
+    };
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    load();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.ledger, state.reloadToken, template, ...(queryDeps ?? [])]);
+  return result;
 }
 
 export type FetchResult<T extends object, K> = {
@@ -70,42 +50,32 @@ export type FetchResult<T extends object, K> = {
 }
 
 /// React Hook for a lookup by key against the `/contracts/lookup` endpoint of the JSON API.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useFetchByKey = <T extends object, K>(template: Template<T, K>, keyFactory: () => K, keyDeps?: readonly any[]): FetchResult<T, K> => {
+export function useFetchByKey<T extends object, K>(template: Template<T, K>, keyFactory: () => K, keyDeps: readonly unknown[]): FetchResult<T, K> {
   const state = useDamlState();
-  const key = useMemo(keyFactory, keyDeps);
-  const contract = LedgerStore.getFetchByKeyResult(state.store, template, key);
+  const [result, setResult] = useState<FetchResult<T, K>>({contract: null, loading: false});
   useEffect(() => {
-    if (contract === undefined) {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      loadFetchByKey(state, template, key);
-    }
-  }, [state, template, key, contract]);
-  return contract ?? TemplateStore.emptyFetchResult();
-}
-
-const reloadTemplate = async <T extends object, K>(state: DamlLedgerState, template: Template<T, K>) => {
-  const templateStore = state.store.templateStores.get(template) as TemplateStore.Store<T, K> | undefined;
-  if (templateStore) {
-    const queries: Query<T>[] = Array.from(templateStore.queryResults.keys());
-    const keys: K[] = Array.from(templateStore.fetchByKeyResults.keys());
-    await Promise.all([
-      Promise.all(queries.map(async (query) => await loadQuery(state, template, query))),
-      Promise.all(keys.map(async (key) => await loadFetchByKey(state, template, key))),
-    ]);
-  }
+    const key = keyFactory();
+    setResult({contract: null, loading: true});
+    const load = async () => {
+      const contract = await state.ledger.fetchByKey(template, key);
+      setResult({contract, loading: false});
+    };
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    load();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.ledger, state.reloadToken, template, ...(keyDeps ?? [])]);
+  return result;
 }
 
 /// React Hook that returns a function to exercise a choice and a boolean
 /// indicator whether the exercise is currently running.
 export const useExercise = <T extends object, C, R>(choice: Choice<T, C, R>): [(cid: ContractId<T>, argument: C) => Promise<R>, boolean] => {
-  const [loading, setLoading] = useState(false);
   const state = useDamlState();
+  const [loading, setLoading] = useState(false);
 
   const exercise = async (cid: ContractId<T>, argument: C) => {
     setLoading(true);
-    const [result, events] = await state.ledger.exercise(choice, cid, argument);
-    state.dispatch(addEvents(events));
+    const [result] = await state.ledger.exercise(choice, cid, argument);
     setLoading(false);
     return result;
   }
@@ -115,24 +85,58 @@ export const useExercise = <T extends object, C, R>(choice: Choice<T, C, R>): [(
 /// React Hook that returns a function to exercise a choice by key and a boolean
 /// indicator whether the exercise is currently running.
 export const useExerciseByKey = <T extends object, C, R, K>(choice: Choice<T, C, R, K>): [(key: K, argument: C) => Promise<R>, boolean] => {
-  const [loading, setLoading] = useState(false);
   const state = useDamlState();
+  const [loading, setLoading] = useState(false);
 
   const exerciseByKey = async (key: K, argument: C) => {
     setLoading(true);
-    const [result, events] = await state.ledger.exerciseByKey(choice, key, argument);
-    state.dispatch(addEvents(events));
+    const [result] = await state.ledger.exerciseByKey(choice, key, argument);
     setLoading(false);
     return result;
   }
   return [exerciseByKey, loading];
 }
 
-/// React Hook to reload all queries currently present in the store.
-export const useReload = (): () => Promise<void> => {
+/// React Hook for a query against the `/contracts/searchForever` endpoint of the JSON API.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useStreamQuery<T extends object, K>(template: Template<T, K>): QueryResult<T, K>
+export function useStreamQuery<T extends object, K>(template: Template<T, K>, queryFactory: () => Query<T>, queryDeps: readonly unknown[]): QueryResult<T, K>
+export function useStreamQuery<T extends object, K>(template: Template<T, K>, queryFactory?: () => Query<T>, queryDeps?: readonly unknown[]): QueryResult<T, K> {
+  const [result, setResult] = useState<QueryResult<T, K>>({contracts: [], loading: false});
   const state = useDamlState();
-  return async () => {
-    const templates = Array.from(state.store.templateStores.keys());
-    await Promise.all(templates.map((template) => reloadTemplate(state, template)));
-  }
+  useEffect(() => {
+    setResult({contracts: [], loading: true});
+    const query = queryFactory ? queryFactory() : undefined;
+    console.log(`mount useStreamQuery(${template.templateId}, ...)`, query);
+    const onEvents = (events: Event<T, K>[]) => setResult(result => {
+      const archiveEvents: Set<ContractId<T>> = new Set();
+      const createEvents: CreateEvent<T, K>[] = [];
+      for (const event of events) {
+        if ('created' in event) {
+          createEvents.push(event.created);
+        } else { // i.e. 'archived' in event
+          archiveEvents.add(event.archived.contractId);
+        }
+      }
+      const contracts = result.contracts
+        .concat(createEvents)
+        .filter(contract => !archiveEvents.has(contract.contractId));
+      return {...result, contracts};
+    });
+    const close = state.ledger.streamQuery(template, onEvents, query);
+    setResult(result => ({...result, loading: false}));
+    const cleanUp = () => {
+      console.log(`unmount useStreamQuery(${template.templateId}, ...)`, query);
+      close();
+    }
+    return cleanUp;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.ledger, template, ...(queryDeps ?? [])]);
+  return result;
+}
+
+/// React Hook to reload all queries currently present in the store.
+export const useReload = (): () => void => {
+  const state = useDamlState();
+  return () => state.triggerReload();
 }
