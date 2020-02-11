1dc5f01d9702b82db95e8aaf4e6de314e530241e
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-11 17:04:19 +0100

    Use daml2ts --main-package-name everywhere (#164)
    I don't want to deal with hashes in my UI code anymore.

--- ui/src/utils/employee.ts
+++ ui/src/utils/employee.ts
@@ -1,7 +1,7 @@
 import { Party } from '@daml/types';
 import { CreateEvent } from '@daml/ledger';
 import { ordString, Ord, contramap } from 'fp-ts/lib/Ord';
-import * as v3 from '@daml2ts/davl-v3/lib/edb5e54da44bc80782890de3fc58edb5cc227a6b7e8c467536f8674b0bf4deb7/DAVL';
+import * as v3 from '@daml2ts/davl-v3/lib/davl-v3/DAVL';
 
 export type EmployeeSummary = {
   employee: Party;
