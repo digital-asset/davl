a8a66e1db90b2df447c935844d80eb4a4abefa8c
Shayne Fletcher <shayne@shaynefletcher.org>
2020-02-17 07:42:17 -0500

    Gather scripts (#192)

--- davl-v4-v5-upgrade.sh
+++ davl-v4-v5-upgrade.sh
@@ -9,11 +9,36 @@ set -eou pipefail
 #   - Accept upgrade proposals.
 #   - Upgrade vacations and requests.
 
+ledger_host=localhost
+ledger_port=6865
 test_setup="`pwd`/test-setup.json"
 test_party="`pwd`/test-party.json"
 
-yarn install && (cd admin-cli && yarn workspaces run build)
+yarn install && \
+ (cd admin-cli && yarn workspaces run build)
 yarn run davl-admin-cli v4-init -f $test_setup
-(cd upgrade-v4-v5/scripts/upgrade-init&&./upgrade-init.sh $test_party)
-(cd upgrade-v4-v5/scripts/upgrade-accept&&./upgrade-accept.sh $test_party)
-(cd upgrade-v4-v5/scripts/upgrade-finish&&./upgrade-finish.sh $test_party)
+(cd upgrade-v4-v5-automation && \
+ daml build -o .daml/dist/automation.dar \
+)
+
+(cd upgrade-v4-v5-automation && \
+  daml script \
+    --dar .daml/dist/automation.dar \
+    --script-name Automation:init \
+    --ledger-host $ledger_host --ledger-port $ledger_port --wall-clock-time \
+    --input-file $test_party \
+)
+(cd upgrade-v4-v5-automation && \
+  daml script \
+    --dar .daml/dist/automation.dar \
+    --script-name Automation:accept \
+    --ledger-host $ledger_host --ledger-port $ledger_port --wall-clock-time \
+    --input-file $test_party \
+ )
+(cd upgrade-v4-v5-automation && \
+  daml trigger \
+    --dar .daml/dist/automation.dar \
+    --trigger-name Automation:finish \
+    --ledger-host $ledger_host --ledger-port $ledger_port --wall-clock-time \
+    --ledger-party "Digital Asset" \
+)
