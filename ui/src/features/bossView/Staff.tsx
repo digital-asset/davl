337ccd8abc790a2d724c9675fc8260d89d16cc81
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-25 18:38:37 +0100

    Make UI ready for upgrade from v4 to v5
    **We cannot deploy this yet since we need to deploy the automation in
    `upgrade-v4-v5-automation` first.**

--- ui/src/features/bossView/Staff.tsx
+++ ui/src/features/bossView/Staff.tsx
@@ -2,16 +2,24 @@ import React from "react";
 import { Segment, Header, List } from "semantic-ui-react";
 import { useStreamQuery, useParty } from "@daml/react";
 import * as v4 from "@daml2ts/davl/lib/davl-0.0.4/DAVL";
+import * as v5 from "@daml2ts/davl/lib/davl-0.0.5/DAVL/V5";
 import { prettyEmployeeSummaries } from "../../utils/employee";
 
 const Staff: React.FC = () => {
   const party = useParty();
 
-  const { loading, contracts } = useStreamQuery(
+  const allocationsV4 = useStreamQuery(
     v4.EmployeeVacationAllocation,
     () => ({ employeeRole: { boss: party } }),
     [party],
   );
+  const allocationsV5 = useStreamQuery(
+    v5.EmployeeVacationAllocation,
+    () => ({ employeeRole: { boss: party } }),
+    [party],
+  );
+  const loading = allocationsV4.loading || allocationsV5.loading;
+  const contracts = allocationsV4.contracts || allocationsV5.contracts;
   const staff = prettyEmployeeSummaries(contracts);
 
   return (
